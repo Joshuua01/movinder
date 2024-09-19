7199b5fa80f4c43f7b792f7917bfe54f28227bb2
Jan Steinbichler <jansteinbichler@wp.pl>
2024-09-19 08:58:31 +0200

    transfer work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,33 @@
-import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <div className="flex h-full w-full flex-col items-center justify-start bg-background">
+      <nav className="my-5 flex w-3/4 items-center justify-between">
+        <h1 className="text-3xl font-bold">Movinder</h1>
+        <div>
+          <Button variant={"default"}>Sign in</Button>
         </div>
-      </div>
-    </main>
+      </nav>
+      <main className="flex w-1/2 flex-1 flex-col items-center justify-center gap-20">
+        <h2 className="text-center text-7xl font-bold">
+          Welcome to the <br /> Movinder application!
+        </h2>
+        <p className="text-center font-medium leading-7 text-muted-foreground">
+          <span className="font-bold">
+            Tired of endless scrolling and indecision?
+          </span>
+          {"  "}
+          Gather your friends, family, or partner and find the perfect movie to
+          watch, effortlessly. With our innovative sliding interface, choosing a
+          movie has never been more fun or collaborative. Simply swipe right on
+          movies you like, left on those you don&apos;t, and let our app do the
+          rest.
+        </p>
+        <Button size={"lg"} variant={"default"}>
+          <span className="text-lg">Start swiping now!</span>
+        </Button>
+      </main>
+    </div>
   );
 }
